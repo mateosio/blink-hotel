2de9f94d06487e9df3702edbafd3af8eda895428
Mateo Sio <siomateo8@gmail.com>
2024-06-26 22:46:56 -0300

    prueba de deploy a vercel

--- backend/src/routes/availability.route.js
+++ backend/src/routes/availability.route.js
@@ -5,8 +5,6 @@ const router = express.Router();
 router.post("/", async (req, res)=>{
     try {
         const dates = req.body;
-        console.log("Llego el rango de fechas a la ruta availability", dates.startDate);
-        console.log("Llego el rango de fechas a la ruta availability", dates.endDate);
         const roomsAvailables = await getAvailables(dates);
         res.status(200).json(roomsAvailables)
     } catch (error) {
