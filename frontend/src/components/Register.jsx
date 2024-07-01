e2d0c80af65edfbbf55e2ccda2f9292f4f639bff
Mateo Sio <siomateo8@gmail.com>
2024-07-01 10:51:49 -0300

    cambio del regex para la contraseña en el componente register

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -5,7 +5,7 @@ import "../index.css";
 import "./register.scss";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z]).{4,24}$/;
 
 const Register = () => {
   const userRef = useRef();
